0bedb39f7c2130f9cfe10645c0134c471ae5c38f
southsanya <121296849+southsanya@users.noreply.github.com>
2023-07-27 20:47:13 +0300

    95

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
 import MainUseParams from './components/Main/MainUseParams';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/login';
 import React from 'react';
+import Preloader from './components/common/preloader/preloader';
+
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+
+// import UsersContainer from './components/Users/UsersContainer';
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 
 
 
@@ -17,20 +22,27 @@ function App(props) {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-      <HeaderContainer />
-      <Side />
-      <div>
-        <Routes>
-        <Route path='/dialogs/*' element={<DialogsContainer/>} />
-        <Route path='/main/*' element={<MainUseParams/>} />
-        <Route path='/news/*' element={<News/>}/>
-        <Route path='/music/*' element={<Music/>}/>
-        <Route path='/settings/*' element={<Settings/>}/>
-        <Route path='/users/*' element={<UsersContainer/>}/>
-        <Route path='/login/*' element={<Login/>}/>
-        </Routes>
+        <HeaderContainer />
+        <Side />
+        <div>
+          <Routes>
+            <Route path='/dialogs/*' element={
+              <React.Suspense fallback={<Preloader/>}>
+                <DialogsContainer />
+              </React.Suspense>
+            } />
+            <Route path='/main/*' element={<MainUseParams />} />
+            <Route path='/news/*' element={<News />} />
+            <Route path='/music/*' element={<Music />} />
+            <Route path='/settings/*' element={<Settings />} />
+            <Route path='/users/*' element={
+              <React.Suspense fallback={<Preloader/>}>
+                <UsersContainer />
+              </React.Suspense>} />
+            <Route path='/login/*' element={<Login />} />
+          </Routes>
+        </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 }
